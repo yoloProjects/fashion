3006f9a37fe25a6f4d8aefaf628ce76f1f9bb6e5
yoloProjects <midhunrajeev2022@gmail.com>
2022-08-04 15:47:57 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,31 +3,70 @@ import ReactDOM from "react-dom";
 import MagicDropzone from "react-magic-dropzone";
 
 import "./styles.css";
-const tf = require('@tensorflow/tfjs');
+const tf = require("@tensorflow/tfjs");
 
-const weights = '/web_model/model.json';
+const weights = "/web_model/model.json";
 
-const names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-               'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-               'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-               'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-               'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-               'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-               'hair drier', 'toothbrush']
+const names = [
+  "shirt, blouse",
+  "top, t-shirt, sweatshirt",
+  "sweater",
+  "cardigan",
+  "jacket",
+  "vest",
+  "pants",
+  "shorts",
+  "skirt",
+  "coat",
+  "dress",
+  "jumpsuit",
+  "cape",
+  "glasses",
+  "hat",
+  "headband, head covering, hair accessory",
+  "tie",
+  "glove",
+  "watch",
+  "belt",
+  "leg warmer",
+  "tights, stockings",
+  "sock",
+  "shoe",
+  "bag, wallet",
+  "scarf",
+  "umbrella",
+  "hood",
+  "collar",
+  "lapel",
+  "epaulette",
+  "sleeve",
+  "pocket",
+  "neckline",
+  "buckle",
+  "zipper",
+  "applique",
+  "bead",
+  "bow",
+  "flower",
+  "fringe",
+  "ribbon",
+  "rivet",
+  "ruffle",
+  "sequin",
+  "tassel",
+];
 
 class App extends React.Component {
   state = {
     model: null,
     preview: "",
-    predictions: []
+    predictions: [],
   };
 
   componentDidMount() {
-    tf.loadGraphModel(weights).then(model => {
+    tf.loadGraphModel(weights).then((model) => {
       this.setState({
-        model: model
+        model: model,
       });
     });
   }
@@ -46,7 +85,10 @@ class App extends React.Component {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    const ratio = Math.min(canvas.width / image.naturalWidth, canvas.height / image.naturalHeight);
+    const ratio = Math.min(
+      canvas.width / image.naturalWidth,
+      canvas.height / image.naturalHeight
+    );
     const newWidth = Math.round(naturalWidth * ratio);
     const newHeight = Math.round(naturalHeight * ratio);
     ctx.drawImage(
@@ -58,21 +100,25 @@ class App extends React.Component {
       (canvas.width - newWidth) / 2,
       (canvas.height - newHeight) / 2,
       newWidth,
-      newHeight,
+      newHeight
     );
-
   };
 
-  onImageChange = e => {
+  onImageChange = (e) => {
     const c = document.getElementById("canvas");
     const ctx = c.getContext("2d");
     this.cropToCanvas(e.target, c, ctx);
-    let [modelWidth, modelHeight] = this.state.model.inputs[0].shape.slice(1, 3);
+    let [modelWidth, modelHeight] = this.state.model.inputs[0].shape.slice(
+      1,
+      3
+    );
     const input = tf.tidy(() => {
-      return tf.image.resizeBilinear(tf.browser.fromPixels(c), [modelWidth, modelHeight])
-        .div(255.0).expandDims(0);
+      return tf.image
+        .resizeBilinear(tf.browser.fromPixels(c), [modelWidth, modelHeight])
+        .div(255.0)
+        .expandDims(0);
     });
-    this.state.model.executeAsync(input).then(res => {
+    this.state.model.executeAsync(input).then((res) => {
       // Font options.
       const font = "16px sans-serif";
       ctx.font = font;
@@ -84,10 +130,10 @@ class App extends React.Component {
       const classes_data = classes.dataSync();
       const valid_detections_data = valid_detections.dataSync()[0];
 
-      tf.dispose(res)
+      tf.dispose(res);
 
       var i;
-      for (i = 0; i < valid_detections_data; ++i){
+      for (i = 0; i < valid_detections_data; ++i) {
         let [x1, y1, x2, y2] = boxes_data.slice(i * 4, (i + 1) * 4);
         x1 *= c.width;
         x2 *= c.width;
@@ -108,10 +154,9 @@ class App extends React.Component {
         const textWidth = ctx.measureText(klass + ":" + score).width;
         const textHeight = parseInt(font, 10); // base 10
         ctx.fillRect(x1, y1, textWidth + 4, textHeight + 4);
-
       }
-      for (i = 0; i < valid_detections_data; ++i){
-        let [x1, y1, , ] = boxes_data.slice(i * 4, (i + 1) * 4);
+      for (i = 0; i < valid_detections_data; ++i) {
+        let [x1, y1, ,] = boxes_data.slice(i * 4, (i + 1) * 4);
         x1 *= c.width;
         y1 *= c.height;
         const klass = names[classes_data[i]];
@@ -120,7 +165,6 @@ class App extends React.Component {
         // Draw the text last to ensure it's on top.
         ctx.fillStyle = "#000000";
         ctx.fillText(klass + ":" + score, x1, y1);
-
       }
     });
   };
