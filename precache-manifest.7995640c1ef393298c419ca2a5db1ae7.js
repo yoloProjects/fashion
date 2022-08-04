8f981e161be23a0f2192285424fee7fbfac23903
yoloProjects <midhunrajeev2022@gmail.com>
2022-08-04 16:34:06 +0530

    Updates

---
+++ precache-manifest.7995640c1ef393298c419ca2a5db1ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8ee4e3cda2adc62781e",
+    "url": "/fashion/static/css/main.d8454c78.chunk.css"
+  },
+  {
+    "revision": "d8ee4e3cda2adc62781e",
+    "url": "/fashion/static/js/main.d8ee4e3c.chunk.js"
+  },
+  {
+    "revision": "fbbb868a410163c5eee8",
+    "url": "/fashion/static/js/1.fbbb868a.chunk.js"
+  },
+  {
+    "revision": "737974e5d02bc3cbebe0",
+    "url": "/fashion/static/js/runtime~main.737974e5.js"
+  },
+  {
+    "revision": "4951f0050c567fa12e06abe4c31e4cc1",
+    "url": "/fashion/index.html"
+  }
+];
